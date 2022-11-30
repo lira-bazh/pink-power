c688507b6f625f4e7439f5d0a31caf8fc4c8ce6e
Irina Bazh <79859948912@ya.ru>
2022-11-30 23:38:12 +0300

    Добавлена линия со знаками

--- client/pages/Main/Main.tsx
+++ client/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { CloudLink, Button, BUTTON_STYLE } from 'atoms'
+import { CloudLink, Button, BUTTON_STYLE, Symbol } from 'atoms'
 import "./style.scss"
 
 export const Main = () => {
@@ -8,28 +8,45 @@ export const Main = () => {
   const services = ["Перейти в PINK", "Интернет-магазин", "PINK ТВ", "Сервисы"]
 
   return (
-    <div className='main-page'>
-        <div className='main-page__header'></div>
-        <div className='main-page__content'>
-          <div className='main-page__links'>
-            ДЛЯ
-            <ul className='devices'>
-              {devices.map((device, index) => {
-                return (<li><CloudLink to="/" title={device}/></li>)
-              })}
-            </ul>
-            <ul className='services'>
-              {services.map((service) => {
-                return (<li><Link to='/'>{service}</Link></li>)
-              })}
-            </ul>
+    <div className="main-page">
+      <div className="main-page__header">
+        {[...Array(22)
+          .keys()]
+          .map((num: number) => {
+            return <Symbol key={`symbol-${num}`} type={num} />;
+          })}
+      </div>
+      <div className="main-page__content">
+        <div className="main-page__links">
+          ДЛЯ
+          <ul className="devices">
+            {devices.map((device, index) => {
+              return (
+                <li>
+                  <CloudLink to="/" title={device} />
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="services">
+            {services.map((service) => {
+              return (
+                <li>
+                  <Link to="/">{service}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="main-page__order">
+          <div className="order-text">
+            С PINK POWER интернет будет с вами везде и всегда
           </div>
-          <div className='main-page__order'>
-            <div className='order-text'>С PINK POWER интернет будет с вами везде и всегда</div>
-            <div className='order-button'><Button style={BUTTON_STYLE.WHITE} title="Заказать SIM-карту"/></div>
+          <div className="order-button">
+            <Button style={BUTTON_STYLE.WHITE} title="Заказать SIM-карту" />
           </div>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
